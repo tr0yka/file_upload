2b00da9e0c3970bc8b7247b32d421f70faa1570c
tr0yka <sergeich06@gmail.com>
2016-03-22 11:36:52 +0300

    Add javascript files

---
+++ js/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('#add_file').click(function(){
+        $('#popup').css({"display":"block"});
+    });
+    $('#close').click(function(){
+        $('#popup').css({"display":"none"});
+    });
+});
